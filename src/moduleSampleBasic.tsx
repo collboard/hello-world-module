01fb79987d9c9cefb5eb9ab341aaed65bee420f1
Pavol Hejny <me@pavolhejny.com>
2021-12-30 12:45:10 +0100

    Draft assets

--- src/moduleSampleBasic.tsx
+++ src/moduleSampleBasic.tsx
@@ -1,6 +1,6 @@
 import { declareModule, ExtraJsxPlace, makeExtrajsxModule } from '@collboard/modules-sdk';
 import * as React from 'react';
-import { StyledIcon } from './StyledIcon';
+import styled from 'styled-components';
 
 declareModule(
     makeExtrajsxModule({
@@ -8,21 +8,30 @@ declareModule(
             name: '@collboard/module-sample-basic',
             title: { en: 'Sample button' },
             categories: ['Productivity', 'Buttons', 'Template'],
-            // TODO: !! Publishing assets: icon: '/assets/icons/copy-material.svg',
+            icon: '/assets/logo.svg',
         },
         place: ExtraJsxPlace.EdgeRight,
         createExtraJsx() {
             return (
-                <button
+                <ButtonElement
                     onClick={async () => {
                         alert(`Hello from Collboard modules!`);
                     }}
                     className="button button-primary button-vertical"
                 >
+                    <img src="/assets/logo" />
                     <span>Hello World! </span>
-                    <StyledIcon />
-                </button>
+                </ButtonElement>
             );
         },
     }),
 );
+
+const ButtonElement = styled.button`
+    background-color: #906090;
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+`;
